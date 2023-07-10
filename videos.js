bd2987c60d7cd0f6e6928f763a54ba154ceb8796
BobisBilly <126552989+BobisBilly@users.noreply.github.com>
2023-07-10 14:20:48 +1200

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,15 +1,11 @@
 
 fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UC1PfLOks6pxvkQAFFp6_gOA&order=date&maxResults=5&key=AIzaSyADqjMbkZz0kgjcuFNzGdY42ve0VSeGfbc")
-.then((response) => response.json())
-  .then((data) => {
-    console.log(data.items);
-    let videos = data.items;
-    let videoContainer = document.querySelector(".youtube-stuff");
-    for (video of videos) {
-      videoContainer.innerHTML += `
-      <h3>${video.snippet.title}</h3>
-      <img src="${video.snippet.thumbnails.default.url}">
-    `
-    }
-  }
-}
+.then((result)=>{
+	return result.json()
+}).then((data)=>{
+	console.log(data)
+	let videos = data.items
+	for (video of videos) {
+		console.log(video.snippet.title)
+	}
+})
